2b4d6664294f1746107c1803ae8c0c22355c9a0f
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-03-20 20:18:07 -0300

    adicionei uma validação para vericar se os parametros recebidos são do tipo numero

--- comparaNumero.js
+++ comparaNumero.js
@@ -10,13 +10,17 @@ exemplo:
  Input: 1, 2Output: Os números 1 e 2 não são iguais. 
 Sua soma é 3, que é menor que 10 e menor que 20.*/
 
+
 const comparaNumero = (a, b) => { 
+    if (typeof a + typeof b === 'numbernumber') {
     let res = a == b? `Os números ${a} e ${b} são iguais`:`Os números ${a} e ${b} não são iguais`;
     let soma = a+b;
     let res2 = soma <= 10? `Sua soma é ${soma}, que é menor que 10`:`Sua soma é ${soma}, que é maior que 10`
     let res3 = soma <= 20? `e menor que 20`:`e maior que 10`
-    
     return console.log(res + ', ' + res2 + ' ' + res3)
+    }else {
+        console.log('Por favor, digite um número válido')
+    }
 }
 
-comparaNumero(2,7); 
+comparaNumero('2',7); 
